222420bb782ff2afe339390e717d40bbe494eb2d
BayElectrician <lachlan.wyer@gmail.com>
2026-04-20 15:06:14 +1000

    Started Data Cleanse

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import pandas as pd
 
-#Testing Comments
 bikeRentalData = pd.read_csv("./bike_rental.csv")
+global df
 df = bikeRentalData
-df.drop_duplicated()
-#print(bikeRentalData[bikeRentalData.isnull().any(axis=1)])
+# Need to save the DataFrame as a new variable for drop to take place
+df = df.drop_duplicates()
 print(df.duplicated().sum())
+print(' ')
+
+
+def fiveNumSummary():
+    stats = pd.DataFrame({
+        'min': round(df.min(numeric_only=True), 2),
+        'max': round(df.max(numeric_only=True), 2),
+        'mean': round(df.mean(numeric_only=True), 2),
+        'median': round(df.median(numeric_only=True), 2),
+        'std': round(df.std(numeric_only=True), 2)
+    })
+    print(stats)
+
+print(df[df.isnull().any(axis=1)])
+
+def fillNull(df):
+    values = {
+        "temp": df['temp'].mean(),
+        "atemp": df['atemp'].mean(),
+        "humidity": df['humidity'].mean()
+    }
+    dfNullFilled=df.fillna(value=values)
+    return dfNullFilled
+    print("Null Values filled")
+
+df = fillNull(df) 
+print(df[df.isnull().any(axis=1)])
+print(fiveNumSummary())
