c731d5ab6d483d9a4f34628743babd48e411d3d0
BayElectrician <lachlan.wyer@gmail.com>
2026-04-21 16:57:06 +1000

    Fixed STDs differences between functions

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
 bikeRentalData = pd.read_csv("./bike_rental.csv")
-global df
-df = bikeRentalData
 # Need to save the DataFrame as a new variable for drop to take place
-df = df.drop_duplicates()
 
-def fiveNumSummary():
+def fiveNumSummary(df):
     stats = pd.DataFrame({
         'min': round(df.min(numeric_only=True), 2),
         'max': round(df.max(numeric_only=True), 2),
@@ -32,20 +29,22 @@ def fixOutliers(df):
         std = df[x].std()
 
         # Using 3stds to gather 99% of all average values
-        lowerBound = mean - 3 * std
+        lowerBound = mean - 2.5 * std
         if lowerBound < 0:
             lowerBound = 0
-        upperBound = mean + 3 * std
+        upperBound = mean + 2.5 * std
 
         df = df[(df[x] >= lowerBound) & (df[x] <= upperBound)]
-        print(df)
+
     return df
 
+def cleanData(df):
+    df = df.drop_duplicates()
+    df = fillNull(df)
+    df = fixOutliers(df)
+    return df
 
-print(round((df.isnull().sum() / df.shape[0]) * 100, 2))
-# df = fillNull(df) 
-# print(df[df.isnull().any(axis=1)])
-df = fixOutliers(df)
-# Do it twice to remove all the outliers
-df = fixOutliers(df)
-print(fiveNumSummary())
+bikeRentalData = cleanData(bikeRentalData)
+print(bikeRentalData)
+print(round((bikeRentalData.isnull().sum() / bikeRentalData.shape[0]) * 100, 2))
+print(fiveNumSummary(bikeRentalData))
